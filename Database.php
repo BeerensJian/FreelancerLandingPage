c3900a9ce8a139dea4d729436dcf60a8761fe34b
Jian Beerens <jian.beerens@numble.be>
2023-08-29 21:46:38 +0200

    Added Database class

---
+++ Database.php
@@ -0,0 +1,34 @@
+<?php
+
+class Database
+{
+    private PDO $connection;
+    private PDOStatement $statement;
+
+    public function __construct(array $config, string $user = "root", string $pass = "root")
+    {
+        extract($config);
+        $this->connection = new PDO(
+            "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset",
+            $user,
+            $pass,
+            [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+        );
+    }
+
+    public function query(string $sql, array $params = []): self
+    {
+        $this->statement = $this->connection->prepare($sql, $params)->execute();
+        return $this;
+    }
+
+    public function fetchAll(): array
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function fetch(): array
+    {
+        return $this->statement->fetch();
+    }
+}
---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '8889',
+        'dbname' => 'elektricityconsulting',
+        'charset' => 'utf8mb4'
+    ]
+];
