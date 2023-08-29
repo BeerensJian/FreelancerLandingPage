de758b5741a8919da620088204cadd4d9ffca351
Jian Beerens <jian.beerens@numble.be>
2023-08-29 21:46:15 +0200

    Initialized router & first route

--- index.php
+++ index.php
@@ -5,3 +5,13 @@ declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+require 'router.php';
+
+$config = require 'config.php';
+$router = new Router($config['database']);
+require 'routes.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+require $router->route($uri, $method);
