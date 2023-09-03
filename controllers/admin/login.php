33f72cc81442d2461ebf8b5ec700d31fe90e9f98
Jian Beerens <jian.beerens@numble.be>
2023-09-03 22:21:27 +0200

    Admin login page

---
+++ controllers/admin/login.php
@@ -0,0 +1,5 @@
+<?php
+
+
+
+require 'views/adminlogin.view.php';
---
+++ views/adminlogin.view.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../public/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg">
+        <div class="login-card">
+            <label for="password"></label>
+            <input type="text" id="password" name="password">
+            <button type="submit">Log In</button>
+        </div>
+    </div>
+
+</body>
+</html>
