6b20244d25e2872096ad589e7650faaeacd62b88
Jian Beerens <jian.beerens@numble.be>
2023-09-06 20:13:42 +0200

    Nav and header

--- views/index.view.php
+++ views/index.view.php
@@ -1,3 +1,27 @@
 <?php
+require 'partials/head.php';
+?>
+<nav>
+    <ul class="container">
+        <li id="title"><h1>Energie Constultant</h1></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Over Ons</a></li>
+        <li><a href="/">Afspraak Maken</a></li>
+    </ul>
+</nav>
 
-echo "index page";
+<header>
+    <div class="header_text container">
+        <h1>Besparen op uw energiefactuur?</h1>
+        <p>Dat is onze expertise!</p>
+    </div>
+
+    <div class="bg_filter">
+
+    </div>
+</header>
+
+
+<?php
+require 'partials/footer.php';
+?>
