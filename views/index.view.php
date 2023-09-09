9782c1b5e2693cc58e60d2f4567ba75de6e07900
Jian Beerens <beerensjian@gmail.com>
2023-09-09 14:04:02 +0200

    Process + values section and start form

--- views/index.view.php
+++ views/index.view.php
@@ -21,8 +21,8 @@ require 'partials/head.php';
 </header>
 
 <section>
+    <h2>Wie ben ik?</h2>
     <div class="container about_me">
-        <h2>Wie ben ik?</h2>
         <div class="about_me_content">
             <p>
                 Hallo! Mijn naam is John Doe, en ik ben uw persoonlijke besparingsadviseur. Als u ooit het gevoel hebt gehad dat u te veel betaalt voor uw maandelijkse rekeningen, ben ik hier om te helpen. Ik weet hoe moeilijk het kan zijn om tijd te vinden om alle verschillende aanbiedingen en tarieven te vergelijken. Dat is waar ik in beeld kom.
@@ -33,16 +33,59 @@ require 'partials/head.php';
 </section>
 
 <section>
+    <h2>Hoe gaan we te werk?</h2>
     <div class="container how_it_works">
-        <h2>Hoe gaan we te werk?</h2>
-        Afspraak Plannen: Neem contact met me op om een geschikt tijdstip te vinden voor onze afspraak.
-        Persoonlijk Bezoek: Ik kom bij u thuis langs voor een grondige analyse van uw huidige contracten en uitgaven.
-        Vergelijking & Aanbevelingen: Na onze ontmoeting zal ik de beste alternatieven voor u opzoeken en aanbevelingen doen die bij uw behoeften passen.
-        Veranderingen Doorvoeren: Indien gewenst, kan ik u helpen met het overschakelen naar een nieuwe leverancier, zodat u meteen kunt beginnen met besparen.
+        <div class="step">
+            <img src="../public/images/edit-list-icon.svg" alt="icon of form and pencil">
+            <h3>Afspraak Plannen</h3>
+            <p>Neem contact met me op om een geschikt tijdstip te vinden voor onze afspraak.</p>
+        </div>
+        <div class="step">
+            <img src="../public/images/chat-message-icon.svg" alt="chatboxes indicating conversation">
+            <h3>Persoonlijk Bezoek</h3>
+            <p>Ik kom bij u thuis langs voor een grondige analyse van uw huidige contracten en uitgaven.</p>
+        </div>
+        <div class="step">
+            <img src="../public/images/reload-sync-icon.svg" alt="comparison icon">
+            <h3>Vergelijking & Aanbevelingen</h3>
+            <p>Na onze ontmoeting zal ik de beste alternatieven voor u opzoeken en aanbevelingen doen die bij uw behoeften passen.</p>
+        </div>
+        <div class="step">
+            <img src="../public/images/party-hat-icon.svg" alt="">
+            <h3>Veranderingen Doorvoeren</h3>
+            <p>Indien gewenst, kan ik u helpen met het overschakelen naar een nieuwe leverancier, zodat u meteen kunt beginnen met besparen.</p>
+        </div>
     </div>
 </section>
 
+<section>
+    <div class="container values">
+        <h2>Waarom zou u voor mij kiezen?</h2>
+        <div class="values_content">
+            <span>PERSOONLIJKE AANPAK</span>
+            <p>Iedere klant is uniek, en ik zorg ervoor dat mijn adviezen op maat gemaakt zijn voor uw specifieke situatie.</p>
+            <span>TRANSPIRANTIE</span>
+            <p> Geen verborgen kosten, geen verrassingen. Ik leg alles duidelijk en eerlijk uit.</p>
+            <span>TIJD & GELD BESPAREN</span>
+            <p> Ik neem het zware werk van u over, zodat u zich kunt focussen op wat echt belangrijk is in uw leven.</p>
+        </div>
+    </div>
+</section>
 
+<section>
+    <form action="">
+        <label for="name">Voor & achternaam:</label>
+        <input type="text" id="name" name="name">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email">
+
+        <label for="gsm">Mobiel nr:</label>
+        <input type="text" id="gsm" name="gsm">
+
+
+    </form>
+</section>
 
 <?php
 require 'partials/footer.php';
