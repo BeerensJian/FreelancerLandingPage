2b5b5e5fde77247dded1c98d648cd37a9bcafb36
Jian Beerens <beerensjian@gmail.com>
2023-09-15 17:29:40 +0200

    Admin login layout, start of logging in admin

--- controllers/admin/session.php
+++ controllers/admin/session.php
@@ -1,1 +1,6 @@
 <?php
+
+// Validate the password
+// check if the password is equal to the password that is set
+// if it is we will sign the admin in with a session
+// redirect to dashboard
--- views/adminlogin.view.php
+++ views/adminlogin.view.php
@@ -5,15 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="../public/css/admin.css">
     <title>Admin Dashboard</title>
 </head>
 <body>
     <div class="bg">
         <div class="login-card">
-            <label for="password"></label>
-            <input type="text" id="password" name="password">
-            <button type="submit">Log In</button>
+            <form action="/adminlogin" method="post">
+                <img src="../public/images/lock-icon.svg" alt="icon of a lock">
+                <label for="password">Password:</label>
+                <input type="text" id="password" name="password">
+                <button type="submit">Log In</button>
+            </form>
         </div>
     </div>
 
