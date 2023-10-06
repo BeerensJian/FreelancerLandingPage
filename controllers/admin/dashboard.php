8f5d1ed2b88deec9ba21f4c3510b08bb9cdfff0d
Jian Beerens <beerensjian@gmail.com>
2023-10-06 19:41:12 +0200

    Show correct items with pagination

--- controllers/admin/dashboard.php
+++ controllers/admin/dashboard.php
@@ -10,8 +10,14 @@ $db = App::resolve("db");
 ["dayTotal" => $day] = $db->query("SELECT count(*) as dayTotal FROM userdata WHERE createdAt > now() - INTERVAL 1 DAY")->fetch();
 ["weekTotal" => $week] = $db->query("SELECT count(*) as weekTotal FROM userdata WHERE createdAt > now() - INTERVAL 1 WEEK")->fetch();
 ["monthTotal" => $month] = $db->query("SELECT count(*) as monthTotal FROM userdata WHERE createdAt > now() - INTERVAL 1 MONTH")->fetch();
+["totalItems" => $totalItems] = $db->query("SELECT count(*) as totalItems FROM userdata;")->fetch();
 
-//last 10 submits
-$userSubmits = $db->query("SELECT * FROM elec_consul.userdata ORDER BY createdAt DESC LIMIT 10;")->fetchAll();
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$itemsPerPage = 10;
+$maxPages = (int) ceil($totalItems / $itemsPerPage);
+$offset = ($page - 1) * $itemsPerPage;
+
+
+$userSubmits = $db->query("SELECT * FROM elec_consul.userdata ORDER BY createdAt DESC LIMIT $offset, $itemsPerPage;")->fetchAll();
 
 require 'views/dashboard.view.php';
